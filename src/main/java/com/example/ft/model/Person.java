5c1d35fcb4a1518e7d084ab9b95e8c9ea42f3aed
Alec Hannaford <alec322@gmail.com>
2023-09-02 16:04:29 -0700

    person model w birthday and date name

--- src/main/java/com/example/ft/model/Person.java
+++ src/main/java/com/example/ft/model/Person.java
@@ -2,6 +2,7 @@ package com.example.ft.model;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
 @Entity
 @Table(name="people")
 public class Person {
@@ -10,5 +11,11 @@ public class Person {
     @SequenceGenerator(name = "person_generator", sequenceName = "people_seq", allocationSize = 1)
     private long id;
 
+    @Column(name = "name")
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birthday")
+    private Date birthday;
 
 }
